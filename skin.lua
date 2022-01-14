747add56f15dd93cd1c85280ee0854814699cae2
Lars Mueller <appgurulars@gmx.de>
2022-01-14 22:08:22 +0100

    Make player_api optional, fallback to properties

--- skin.lua
+++ skin.lua
@@ -1,14 +1,37 @@
+local player_api = rawget(_G, "player_api")
+
 local function get_textures(player)
-	local anim = player_api.get_animation(player)
-	return anim.textures or player_api.registered_models[anim.model].textures
+	if player_api then
+		local anim = player_api.get_animation(player)
+		return anim.textures or player_api.registered_models[anim.model].textures
+	end
+	return player:get_properties().textures
 end
 
+local function get_texture(player, index)
+	return assert(get_textures(player)[index])
+end
+
+local function set_textures(player, textures)
+	if player_api then
+		player_api.set_textures(player, textures)
+		return
+	end
+	player:set_properties{textures = textures}
+end
+
+local function set_texture(player, index, texture)
+	local textures = modlib.table.copy(get_textures(player))
+	textures[index] = texture
+	set_textures(player, textures)
+end
+
+local skin_texture_index = 1
+
 function epidermis.get_skin(player)
-	return get_textures(player)[1]
+	return get_texture(player, skin_texture_index)
 end
 
 function epidermis.set_skin(player, skin)
-	local textures = modlib.table.copy(get_textures(player))
-	textures[1] = skin
-	player_api.set_textures(player, textures)
+	set_texture(player, skin_texture_index, skin)
 end
