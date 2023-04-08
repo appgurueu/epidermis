4a3a347c42ac5304c1c6f4b03f304db8457b7105
Lars Mueller <appgurulars@gmx.de>
2023-04-08 17:54:47 +0200

    Fix dynamic media waiting on players who left
    Untested.

--- dynamic_add_media.lua
+++ dynamic_add_media.lua
@@ -1,4 +1,5 @@
 local media_paths = epidermis.media_paths
+local on_receives = {} -- set of `function on_receive(name)`
 -- TODO keep count of total added media:
 -- Force-kick players after their RAM is too full
 -- Restart after server disk is too full
@@ -37,24 +38,36 @@ function epidermis.dynamic_add_media(path, on_all_received, ephemeral)
 
 	if not next(to_receive) then
 		minetest.dynamic_add_media(arg, function(name)
-			minetest.log("warning", ("%s received media %s despite not being connected"):format(name, filename))
+			minetest.log("warning", ("%s received media %s despite not being connected"):format(name or "All players", filename))
 		end)
 		on_all_received()
 		return
 	end
 
-	minetest.dynamic_add_media(arg, function(name)
-		if name == nil then
-			on_all_received()
-			return
-		end
-		if not to_receive[name] then
-			minetest.log("warning", ("%s received media %s despite not being connected"):format(name, filename))
-			return
-		end
+	local function on_receive(name)
 		to_receive[name] = nil
 		if not next(to_receive) then
+			on_receives[on_receive] = nil
 			on_all_received()
+		end
+	end
+	on_receives[on_receive] = true
+	minetest.dynamic_add_media(arg, function(name)
+		if name == nil then -- pre-5.5 => all players have received the media
+			on_receives[on_receive] = nil
+			on_all_received()
+		elseif to_receive[name] then
+			on_receive(name)
+		else
+			minetest.log("warning", ("%s received media %s despite not being connected"):format(name, filename))
 		end
 	end)
 end
+
+-- Don't wait for leaving players to receive media.
+minetest.register_on_leaveplayer(function(player)
+	local name = player:get_player_name()
+	for on_receive in pairs(on_receives) do
+		on_receive(name)
+	end
+end)
